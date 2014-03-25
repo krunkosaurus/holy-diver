d69bf367a85eb1d11d43df2547c0a126414ec268
Dustin Larimer <dustinlarimer@gmail.com>
2014-03-25 16:02:57 -0700

    tabdeath

--- holy-diver.js
+++ holy-diver.js
@@ -1,19 +1,19 @@
 // by underscore.js!
 var _each = function(obj, iterator, context) {
-	if (obj == null) return obj;
-	if (Array.prototype.forEach && obj.forEach === Array.prototype.forEach) {
-		obj.forEach(iterator, context);
-	} else if (obj.length === +obj.length) {
-		for (var i = 0, length = obj.length; i < length; i++) {
-			if (iterator.call(context, obj[i], i, obj) === breaker) return;
-		}
-	} else {
-		var keys = _.keys(obj);
-		for (var i = 0, length = keys.length; i < length; i++) {
-			if (iterator.call(context, obj[keys[i]], keys[i], obj) === breaker) return;
-		}
-	}
-	return obj;
+  if (obj == null) return obj;
+  if (Array.prototype.forEach && obj.forEach === Array.prototype.forEach) {
+    obj.forEach(iterator, context);
+  } else if (obj.length === +obj.length) {
+    for (var i = 0, length = obj.length; i < length; i++) {
+      if (iterator.call(context, obj[i], i, obj) === breaker) return;
+    }
+  } else {
+    var keys = _.keys(obj);
+    for (var i = 0, length = keys.length; i < length; i++) {
+      if (iterator.call(context, obj[keys[i]], keys[i], obj) === breaker) return;
+    }
+  }
+  return obj;
 };
 
 function flatten(root, setup) {
@@ -31,7 +31,7 @@ flatten.prototype.build = function(root, setup) {
     cells: setup.rows.cells.split(" -> ")
   };
   self.table = [];
-	self.series = [];
+  self.series = [];
   self.raw = root;
   
   // Header (Series)
@@ -39,9 +39,9 @@ flatten.prototype.build = function(root, setup) {
     var index = [self.cols.index];
     var cells = parse.apply(self, [root[0]].concat(self.cols.cells));
     self.table.push(index.concat(cells));
-		_each(cells, function(el, index){
-			self.series.push({ key: el, values: [] });
-		});
+    _each(cells, function(el, index){
+      self.series.push({ key: el, values: [] });
+    });
   })();
   
   // Rows
@@ -49,17 +49,17 @@ flatten.prototype.build = function(root, setup) {
     var index = parse.apply(self, [el].concat(self.rows.index));
     var cells = parse.apply(self, [el].concat(self.rows.cells));
     self.table.push(index.concat(cells));
-		_each(self.series, function(series, j){
-			series['values'].push(cells[j]);
-		});
+    _each(self.series, function(series, j){
+      series['values'].push(cells[j]);
+    });
   });
   
   return this;
 };
 flatten.prototype.render = function(format){
-	if (format == 'csv') {
-		console.log(this.table.join('\n'));
-	}
+  if (format == 'csv') {
+    console.log(this.table.join('\n'));
+  }
 };
 
 
